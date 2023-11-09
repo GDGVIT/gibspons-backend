4e2dabc823d05b77234d98ea6406289035fe1ce4
Soham Ghugare <soham.ghugare@gmail.com>
2023-11-09 19:30:22 +0530

    feat: added user schema

---
+++ api/database/tables/users.go
@@ -0,0 +1,9 @@
+package tables
+
+type Users struct {
+	ID       int    `gorm:"column:user_id;primaryKey;autoIncrement"`
+	Name     string `gorm:"column:user_name;not null;type:varchar(100)"`
+	Email    string `gorm:"column:user_email;unique;type:varchar(100)"`
+	Phone    string `gorm:"column:user_phone;not null;type:varchar(10)"`
+	Password string `gorm:"column:user_password;not null"`
+}
