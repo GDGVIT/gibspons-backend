1f13f33d11d5609b9aba9cbf9194a2e28fa0da4e
Soham Ghugare <soham.ghugare@gmail.com>
2023-12-22 18:14:28 +0530

    feat: added salting

--- api/utility/passwords.go
+++ api/utility/passwords.go
@@ -1,25 +1,41 @@
 package utility
 
 import (
+	"crypto/rand"
 	"errors"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
 func HashPassword(password string) (string, error) {
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	saltSize := 10
+	salt := make([]byte, saltSize)
+	if _, err := rand.Read(salt); err != nil {
+		return "", err
+	}
+	hash, err := bcrypt.GenerateFromPassword([]byte(password+string(salt)), bcrypt.DefaultCost)
 	if err != nil {
 		return "", err
 	}
-	return string(hash), nil
+	return string(append(salt, hash...)), nil
 }
 
-func VerifyPassword(hash string, password string) (bool, error) {
+func VerifyPassword(hashedPassword string, password string) (bool, error) {
+	saltSize := 10
+	hash := hashedPassword
+
 	if len(hash) < bcrypt.DefaultCost {
 		return false, errors.New("invalid hash")
 	}
 
-	if err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)); err != nil {
+	// Extract the salt from the hash
+	if len(hash) < saltSize+bcrypt.MinCost {
+		return false, errors.New("invalid hash")
+	}
+	salt := hash[:saltSize]
+	hashWithoutSalt := hash[saltSize:]
+
+	if err := bcrypt.CompareHashAndPassword([]byte(hashWithoutSalt), []byte(password+string(salt))); err != nil {
 		return false, err
 	}
 
