12c70fd3fe038d650ba895d9ff276d899e3412f8
Soham Ghugare <soham.ghugare@gmail.com>
2023-12-22 18:10:24 +0530

    fix: fixed column names

--- api/database/tables/updates.go
+++ api/database/tables/updates.go
@@ -1,7 +1,7 @@
 package tables
 
 type Updates struct {
-	ID     int    `gorm:"column:user_id;primaryKey;autoIncrement"`
-	Status string `gorm:"column:user_name;not null;type:varchar(50)"`
-	Email  string `gorm:"column:user_email;unique;type:varchar(100)"`
+	ID     int    `gorm:"column:id;primaryKey;autoIncrement"`
+	Status string `gorm:"column:status;not null;type:varchar(50)"`
+	Email  string `gorm:"column:email;unique;type:varchar(100)"`
 }
